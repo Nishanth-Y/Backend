f3571afbcdbc78644c162a6dad5141f5371cf9b2
Nishanth-Y <nishanthy1411@gmail.com>
2024-04-04 21:09:27 +0530

    changes vercel 1

--- server.js
+++ server.js
@@ -18,15 +18,9 @@ app.use("/api/admin", require("./routes/api/admin"));
 app.use("/api/upload", require("./routes/api/images"));
 app.use("/api/profile", require("./routes/api/profile"));
 
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
-
+app.get("/", (req, res) => {
+  res.send("Express Started....");
+});
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
