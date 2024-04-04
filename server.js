c30e14a39845c72714b7ab98ffa6af2e8d53f533
Nishanth-Y <nishanthy1411@gmail.com>
2024-04-04 20:48:05 +0530

    changes vercel

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const connectDB = require('./config/db');
-const path = require('path');
+const express = require("express");
+const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 
@@ -10,23 +10,23 @@ connectDB();
 // Init Middleware
 app.use(express.json());
 
-app.use('/uploads', express.static('uploads'));
+app.use("/uploads", express.static("uploads"));
 
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/admin', require('./routes/api/admin'));
-app.use('/api/upload', require('./routes/api/images'));
-app.use('/api/profile', require('./routes/api/profile'));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/admin", require("./routes/api/admin"));
+app.use("/api/upload", require("./routes/api/images"));
+app.use("/api/profile", require("./routes/api/profile"));
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static("client/build"));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
-const PORT = process.env.PORT || 6001;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
